2df789d2d2676af3b88ece836c716ab917ed7fd7
booleandev <boolean.yan@gmail.com>
2022-05-10 16:25:07 +0800

    feat: init project

---
+++ src/main/java/com/booleandev/picbed/PicBedApplication.java
@@ -0,0 +1,13 @@
+package com.booleandev.picbed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicBedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicBedApplication.class, args);
+    }
+
+}
